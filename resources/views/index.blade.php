7b84a542e0686536a5fc668371abc449c639f0e2
TaqwaAlkhlefat <126411392+TaqwaAlkhlefat@users.noreply.github.com>
2023-06-01 16:27:36 +0300

    I just add comment "hi there"

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.master')
 @section('content')
 <!-- banner section start -->
+{{-- hi there --}}
       <div class="banner_section layout_padding">
          <div class="container">
             <div class="row">
